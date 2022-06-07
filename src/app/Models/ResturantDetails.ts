e3936e55a95eeff07eeaa8e874dbe1995a91808f
esraa267 <esraa.mohamed.fcis@gmail.com>
2022-06-07 23:55:08 +0200

    second

--- src/app/Models/ResturantDetails.ts
+++ src/app/Models/ResturantDetails.ts
@@ -1,9 +1,70 @@
 export interface ResturantDetails {
-  branches?: [];
-  extras?: [];
-  favourites?: [];
-  menu?: any;
-  offers?: [];
-  rates?: [];
-  restaurant?: any;
+  menu?: [
+    {
+      image: string;
+      name: string;
+      products: [
+        {
+          name: string;
+          description: string;
+          price: string;
+          video: string;
+          icon: string;
+          images: string;
+          sizes: string;
+          rate_count: number;
+          model_type: string;
+        }
+      ];
+    }
+  ];
+  restaurant?: {
+    name: string;
+    description: string;
+    logo: string;
+    phone: string;
+
+    governorate: {
+      name: string;
+    };
+    region: {
+      region_id: string;
+    };
+    address: string;
+
+    rate_count: string;
+  };
+  offers: [
+    {
+      name: string;
+      price: string;
+      video: string;
+      images: string;
+      restaurant_id: string;
+      restaurant_name: string;
+      restaurant_logo: string;
+      products: [
+        {
+          quantity: string;
+          product_id: string;
+          product: string;
+          icon: string;
+        }
+      ];
+      rate: string;
+      rate_count: string;
+      model_type: string;
+      qty_cart: string;
+    }
+  ];
+  extras: [
+    {
+      id: string;
+      extra_id: string;
+      name: string;
+      price: string;
+      image: string;
+      qty_cart: number;
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/Layout/navbar/navbar.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { InterceptorService } from './service/Interceptor/interceptor.service';
+import { ColorDirective } from './Directives/color/color.directive';
+import { MatTabsModule } from '@angular/material/tabs';
+import { RatingModule } from 'ngx-bootstrap/rating';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FormsModule } from '@angular/forms';
 import { ResturantComponent } from './components/resturant/resturant.component';
 import { ResturantDetailsComponent } from './components/resturant/resturant-details/resturant-details.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RatingModule } from 'ngx-bootstrap/rating';
-import { FormsModule } from '@angular/forms';
-import { InterceptorService } from './service/Interceptor/interceptor.service';
 import { MenuComponent } from './components/resturant/menu/menu.component';
 import { ExtraComponent } from './components/resturant/extra/extra.component';
 import { OffersComponent } from './components/resturant/offers/offers.component';
-import { MatTabsModule } from '@angular/material/tabs';
-import { ColorDirective } from './Directives/color/color.directive';
 import { SpinnerComponent } from './components/Layout/spinner/spinner.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    ColorDirective,
     NavbarComponent,
     ResturantComponent,
     ResturantDetailsComponent,
     MenuComponent,
     ExtraComponent,
     OffersComponent,
-    ColorDirective,
     SpinnerComponent,
   ],
   imports: [
     BrowserModule,
-    MatProgressSpinnerModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    RatingModule.forRoot(),
-    FormsModule,
     MatTabsModule,
+    RatingModule.forRoot(),
+    MatProgressSpinnerModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true },
